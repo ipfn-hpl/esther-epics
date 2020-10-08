a829e64669ae2f46754e106e5f355a8258d7df77
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2020-10-08 12:52:47 +0100

    edit scripts. opi

--- scripts/launch-cagateway.sh
+++ scripts/launch-cagateway.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#https://epics.anl.gov/EpicsDocumentation/ExtensionsManuals/Gateway/Gateway.html#Starting
+# https://epics.anl.gov/EpicsDocumentation/ExtensionsManuals/Gateway/Gateway.html#Starting
 EPICS_ROOT=/opt/epics7
 EPICS_BASE=${EPICS_ROOT}/epics-base
 EPICS_HOST_ARCH=`${EPICS_BASE}/startup/EpicsHostArch`
 CA_GATE_BIN=${EPICS_ROOT}/support/ca-gateway/bin/linux-x86_64
-${CA_GATE_BIN}/gateway -log gateway.log -cip 192.168.0.255 -sip 10.10.136.78 -server  -archive -no_cache
+${CA_GATE_BIN}/gateway -log gateway.log -cip 192.168.0.255 -sip 10.10.136.73 -server  -archive -no_cache
