106706986088cad01a561509bfcb515b5e5c1959
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2021-06-18 17:57:19 +0100

    edit OPI

--- launch-scripts/launch-cagateway.sh
+++ launch-scripts/launch-cagateway.sh
@@ -4,4 +4,6 @@ EPICS_ROOT=/opt/epics7
 EPICS_BASE=${EPICS_ROOT}/epics-base
 EPICS_HOST_ARCH=`${EPICS_BASE}/startup/EpicsHostArch`
 CA_GATE_BIN=${EPICS_ROOT}/support/ca-gateway/bin/linux-x86_64
-${CA_GATE_BIN}/gateway -log gateway.log -cip 192.168.0.255 -sip 10.10.136.73 -server  -archive -no_cache
+MY_IP=$(/sbin/ip -o -4 addr list enp3s0f3 | awk '{print $4}' | cut -d/ -f1)
+echo $MY_IP
+${CA_GATE_BIN}/gateway -log gateway.log -cip 192.168.0.255 -sip ${MY_IP} -server  -archive -no_cache
