d4a4f9b67529b00ca89987f8965072555ab4a2b3
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2021-09-07 14:05:26 +0100

    changed to epics7

--- iocs/estherGas/iocBoot/iocestherGas/st.cmd
+++ iocs/estherGas/iocBoot/iocestherGas/st.cmd
@@ -1,11 +1,4 @@
-#!./bin/linux-arm/estherGas
-#Tested on 
-# Linux raspberrypi 4.19.66-v7+ 
-# linux-arm kernel 3.18.7-v7+
-#
-#s7nodave-2.1.3
-#asyn4-35
-#epics base-3.15.6
+#!../../bin/linux-arm/estherGas
 
 ## You may have to change estherGas to something else
 ## everywhere it appears in this file
@@ -20,10 +13,11 @@ estherGas_registerRecordDeviceDriver pdbbase
 
 #Configure PLC Connection
 s7nodaveConfigureIsoTcpPort("myPLC", "192.168.0.2", 0)
- 
+
 # Configure Poll Group
 s7nodaveConfigurePollGroup("myPLC", "default", 1.0, 0)
 s7nodaveConfigurePollGroup("myPLC", "fast", 0.1, 0)
+
 
 ## Load record instances
 dbLoadRecords("db/S7Tags.db","user=Esther:gas")
@@ -35,14 +29,17 @@ dbLoadRecords("db/S7sbits.db","user=Esther:gas, plc=myPLC")
 dbLoadRecords("db/S7MFC.db","user=Esther:gas, plc=myPLC")
 dbLoadRecords("db/S7PV70xAlarms.db","user=Esther:gas, plc=myPLC")
 
+
+## Set this to see messages from mySub
+#var mySubDebug 1
+
+## Run this to trace the stages of iocInit
+#traceIocInit
+
 asSetFilename("/opt/epics/iocs/estherGas/access_critical.acf")
 
-#cd ${TOP}/iocBoot/${IOC}
-#cd ${TOP}
+cd ${TOP}/iocBoot/${IOC}
 iocInit
 
-#IOC access security
-asInit
-
 ## Start any sequence programs
-#seq sncxxx,"user=bernardoHost"
+#seq sncExample, "user=piHost"
