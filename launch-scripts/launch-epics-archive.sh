dc93b901730e8a51a39599e360f85c82c674ed19
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2022-04-22 13:17:58 +0100

    Changed Phoebus displays
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- launch-scripts/launch-epics-archive.sh
+++ launch-scripts/launch-epics-archive.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #
-cd /home/esther/EPICS/archive-engine-4.1.0
-./ArchiveEngine -pluginCustomization esther.ini -engine estherPulse -port 4812&
-sleep 2s
+# To import engine:
+# /opt/epics7/archive-engine-4.6.6/archive-engine.sh -settings esther.ini -engine estherPulse -delete_config
+# /opt/epics7/archive-engine-4.6.6/archive-engine.sh -settings esther.ini -engine estherPulse -import estherEngineConfigPulse.xml
+#
+JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+PATH=$JAVA_HOME/bin:$PATH
+/opt/epics7/archive-engine-4.6.6/archive-engine.sh -engine estherPulse -port 4812 -settings esther.ini &
+
 echo "Archive Started. Check browser at  http://localhost:4812/main"
+
