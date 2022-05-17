e904f2da5dc4f92314105d0d8496c15111c11251
Carvalho, Bernardo (bcar) <bernardo.carvalho@tecnico.ulisboa.pt>
2022-05-17 15:45:08 +0100

    Added CSS-Archive scripts and systemd service
    Signed-off-by: Carvalho, Bernardo (bcar) <bernardo.carvalho@tecnico.ulisboa.pt>

---
+++ css-archive-engine/epics-archive.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# To replace engine:
+# /opt/epics7/archive-engine-4.6.6/archive-engine.sh -settings esther.ini -engine estherPulse -delete_config
+# /opt/epics7/archive-engine-4.6.6/archive-engine.sh -settings esther.ini -engine estherPulse -import estherEngineConfigPulse.xml
+#
+JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+PATH=$JAVA_HOME/bin:$PATH
+CSS_HOME="/opt/css-phoebus/archive-engine-4.6.6"
+# CSS_ARCH_HOME=/home/oper/CSS-Archive/archive-engine-4.6.6
+#/archive-engine.sh -engine estherPulse -port 4812 -settings esther.ini &
+
+TOP="/home/bernardo/EPICS/esther-epics/css-archive-engine"
+#TOP="/home/bernardo/EPICS/esther-epics/css-archive-engines-archive-engine"
+cd $TOP
+#JAR=`echo "${TOP}/service-archive-engine-*.jar"`
+#JAR=$(ls service-archive-engine-*.jar)
+JAR=$CSS_HOME/service-archive-engine-4.6.6.jar
+#$JAVA_HOME/bin/java -jar $JAR $OPT "$@"
+$JAVA_HOME/bin/java -jar $JAR  -engine estherPulse -port 4812 -settings esther.ini -noshell
+
+#sleep 2s
+
+echo "Archive Started. Check browser at  http://localhost:4812/main"
+
