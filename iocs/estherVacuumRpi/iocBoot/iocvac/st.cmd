998a2171bb2c420de1f5e3f8bb5a8a4c9ded5bd7
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2022-06-03 17:49:34 +0100

    compiling
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- iocs/estherVacuumRpi/iocBoot/iocvac/st.cmd
+++ iocs/estherVacuumRpi/iocBoot/iocvac/st.cmd
@@ -15,63 +15,34 @@ dbLoadDatabase "dbd/vac.dbd"
 vac_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
-#dbLoadRecords("db/xxx.db","user=pi")
-
-## EDWARDS SCU 800
-## Load Serial drivers
-drvAsynSerialPortConfigure("RS485","/dev/rs485")
-asynSetOption("RS485", 0, "baud", "38400")
-asynSetOption("RS485", 0, "bits", "8")
-asynSetOption("RS485", 0, "parity", "none")
-asynSetOption("RS485", 0, "stop", "1")
-#asynSetOption("RS485", 0, "clocal", "Y")
-#asynSetOption("RS485", 0, "crtscts", "N")
-## Load record instances
-dbLoadRecords("db/edwards.db", "P=Esther:,R=EDW:,BUS=RS485")
+#dbLoadTemplate "db/user.substitutions"
+dbLoadRecords "db/vacVersion.db", "user=pi"
+#dbLoadRecords "db/dbSubExample.db", "user=pi"
 
 ## EDWARDS ADC
-drvAsynSerialPortConfigure("RS232E1","/dev/edwardsADC")
-asynSetOption("RS232E1", 0, "baud", "9600")
-asynSetOption("RS232E1", 0, "bits", "8")
-asynSetOption("RS232E1", 0, "parity", "none")
-asynSetOption("RS232E1", 0, "stop", "1")
+#drvAsynSerialPortConfigure("RS232E1","/dev/edwardsADC")
+#asynSetOption("RS232E1", 0, "baud", "9600")
+#asynSetOption("RS232E1", 0, "bits", "8")
+#asynSetOption("RS232E1", 0, "parity", "none")
+#asynSetOption("RS232E1", 0, "stop", "1")
 ##asynSetOption("RS232E1", 0, "clocal", "Y")
 ##asynSetOption("RS232E1", 0, "crtscts", "N")
-dbLoadRecords("db/edwards-adc.db", "P=Esther:,R=Vacuum:,A=1,BUS=RS232E1")
+#dbLoadRecords("db/edwards-adc.db", "P=Esther:,R=Vacuum:,A=1,BUS=RS232E1")
 
-# Arduino MST12 ARM control CTST
-#drvAsynSerialPortConfigure("RS232A1","/dev/armCTST")
-drvAsynSerialPortConfigure("RS232A1","/dev/ttyACM0")
-asynSetOption("RS232A1", 0, "baud", "115200")
-asynSetOption("RS232A1", 0, "bits", "8")
-asynSetOption("RS232A1", 0, "parity", "none")
-asynSetOption("RS232A1", 0, "stop", "1")
-dbLoadRecords("db/armcontrol.db", "P=Esther:,R=ARM:,A=1")
+dbLoadRecords("db/estherStates.db", "P=Esther:,R=Vacuum:")
 
-# No power on Raspberry USB for SeeduinoV4.2
-#epicsEnvSet "A" "$(A=3)"
-#epicsEnvSet "BRS" "$(BRS=RS232A$(A))"
-## Arduino HVA Gate Valve control
-#drvAsynSerialPortConfigure("$(BRS)","/dev/gatevalveCTST",0,0,0)
-#asynSetOption("$(BRS)", 0, "baud", "115200")
-#asynSetOption("$(BRS)", 0, "bits", "8")
-#asynSetOption("$(BRS)", 0, "parity", "none")
-#asynSetOption("$(BRS)", 0, "stop", "1")
-#dbLoadRecords("db/armcontrol.db", "P=$(P):,R=HVA:,A=3")
+#- Set this to see messages from mySub
+#var mySubDebug 1
 
-## Arduino Gate Valve  control
-#epicsEnvSet "E" "$(E=4)"
-#epicsEnvSet "BRE" "$(BRE=RS232A$(E))"
-## Arduino HVA Gate Valve control
-#drvAsynSerialPortConfigure("$(BRE)","/dev/gatevalveSTDT",0,0,0)
-#asynSetOption("$(BRE)", 0, "baud", "115200")
-#asynSetOption("$(BRE)", 0, "bits", "8")
-#asynSetOption("$(BRE)", 0, "parity", "none")
-#asynSetOption("$(BRE)", 0, "stop", "1")
-#dbLoadRecords("db/armcontrol.db", "P=$(P):,R=HVA:,A=$(E)")
+#- Run this to trace the stages of iocInit
+#traceIocInit
+
+var streamError 1
+#var streamDebug 1
+#streamSetLogfile("stream_logfile.txt")
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
 
 ## Start any sequence programs
-#seq sncxxx,"user=pi"
+seq sncEstherVacuum, "user=pi"
