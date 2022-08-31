c44374cecd1143c43ac968e36ef4bf9676318b03
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2022-08-31 14:48:31 +0100

    Adding modbus support
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- iocs/estherVacuumRpi4/iocBoot/iocvac/st.cmd
+++ iocs/estherVacuumRpi4/iocBoot/iocvac/st.cmd
@@ -68,6 +68,38 @@ dbLoadRecords("db/edwards-adc.db", "P=Esther:,R=Vacuum:,A=1,BUS=RS232E1")
 
 dbLoadRecords("db/estherStates.db", "P=Esther:,R=Vacuum:")
 
+drvAsynSerialPortConfigure("MDBUS", "/dev/modbus", 0, 0, 0)
+asynSetOption("MDBUS",0,"baud","19200")
+asynSetOption("MDBUS",0,"parity","even")
+asynSetOption("MDBUS",0,"bits","8")
+asynSetOption("MDBUS",0,"stop","1")
+
+# modbusInterposeConfig(portName,
+#                      linkType,
+#                      timeoutMsec,
+#                      writeDelayMsec)
+# Modbus link layer type:, 0 = TCP/IP, 1 = RTU, 2 = ASCII
+
+modbusInterposeConfig("MDBUS",1,1000,0)
+# drvModbusAsynConfigure(portName,
+#                       tcpPortName,
+#                       slaveAddress,
+#                       modbusFunction,
+#                       modbusStartAddress,
+#                       modbusLength,
+#                       dataType,
+#                       pollMsec,
+#                       plcType);
+# Read  words (16 bits).  Function code=3 PDU address (start=0).
+# USE PDU ADDRESSES, prefix 0 is octal
+drvModbusAsynConfigure("K1_Y32_In_Word","MDBUS", 1, 3, 32, 1,    0,  100, "el-flow")
+## Load record instances
+dbLoadRecords("db/mb-li.db","P='Esther:MFC1',R=Measure,PORT=K1_Y32_In_Word,OFFSET=0,SCAN='I/O Intr'")
+
+drvModbusAsynConfigure("K1_Y1063_In_Word","MDBUS", 1, 3, 1063, 1,    0,  100, "el-flow")
+dbLoadRecords("db/mb-li.db","P='Esther:MFC1',R=Temperature,PORT=K1_Y1063_In_Word,OFFSET=0,SCAN='I/O Intr'")
+
+
 #- Set this to see messages from mySub
 #var mySubDebug 1
 
