3839f838834e809e89d917258d36b886ae30adf7
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2023-01-14 21:39:41 +0000

    Added links to DBWR
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- launch-scripts/launch-cagateway.sh
+++ launch-scripts/launch-cagateway.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # https://epics.anl.gov/EpicsDocumentation/ExtensionsManuals/Gateway/Gateway.html#Starting
-EPICS_ROOT=/opt/epics7
+EPICS_ROOT=/opt/epics
 EPICS_BASE=${EPICS_ROOT}/epics-base
 EPICS_HOST_ARCH=`${EPICS_BASE}/startup/EpicsHostArch`
-CA_GATE_BIN=${EPICS_ROOT}/support/ca-gateway/bin/linux-x86_64
-MY_IP=$(/sbin/ip -o -4 addr list enp3s0f3 | awk '{print $4}' | cut -d/ -f1)
+CA_GATE_BIN=${EPICS_ROOT}/extensions/ca-gateway/bin/linux-x86_64
+MY_IP=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 echo $MY_IP
-${CA_GATE_BIN}/gateway -log gateway.log -cip 192.168.0.255 -sip ${MY_IP} -server  -archive -no_cache
+${CA_GATE_BIN}/gateway -log /tmp/gateway.log -cip 192.168.0.255 -sip ${MY_IP} -server  -archive -no_cache
---
+++ phoebus-display-builder/dbwr-links.md
@@ -0,0 +1,14 @@
+http://epics.ipfn.tecnico.ulisboa.pt:8080/dbwr/view.jsp?display=http://localhost/displays/isttok.bob
+
+http://10.10.136.128:8080/manager/html
+
+http://10.10.136.128:8080/dbwr/
+
+http://10.10.136.128:8080/dbwr/view.jsp?display=http://localhost/displays/VacuumStates.bob
+
+http://10.10.136.128:8080/dbwr/view.jsp?display=http://localhost/CSS/VacuumStates.bob
+
+http://10.10.136.128:8080/dbwr/view.jsp?display=http://localhost/CSS/EstherVacuum.bob
+
+http://10.10.136.128:8080/dbwr/view.jsp?display=https://raw.githubusercontent.com/ipfn-hpl/esther-epics/master/phoebus-display-builder/CSS/gas_set_points.bob
+
