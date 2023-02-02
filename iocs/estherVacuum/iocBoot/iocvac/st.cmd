13de6910bc677f11e35749ec585576b700113eaa
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2023-02-02 14:10:45 +0000

    Changed Inficon IOC
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- iocs/estherVacuum/iocBoot/iocvac/st.cmd
+++ iocs/estherVacuum/iocBoot/iocvac/st.cmd
@@ -5,6 +5,7 @@
 
 < envPaths
 
+epicsEnvSet("STREAM_PROTOCOL_PATH", "$(TOP)/db")
 cd "${TOP}"
 
 ## Register all support components
@@ -12,10 +13,23 @@ dbLoadDatabase "dbd/vac.dbd"
 vac_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
-#dbLoadRecords("db/xxx.db","user=esther")
+drvAsynSerialPortConfigure("RS232INF","/dev/tty_inficon")
+asynSetOption("RS232INF", 0, "baud", "19200")
+asynSetOption("RS232INF", 0, "bits", "8")
+asynSetOption("RS232INF", 0, "parity", "none")
+asynSetOption("RS232INF", 0, "stop", "1")
+dbLoadRecords("db/inficonVG401.db", "P=Esther:,R=Vacuum:,BUS=RS232INF")
+
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
+
+var streamError 1
+var streamDebug 0
+var streamDebugColored 1
+var streamErrorDeadTime 30
+var streamMsgTimeStamped 1
+streamSetLogfile("logfile.txt")
 
 ## Start any sequence programs
 #seq sncxxx,"user=esther"
