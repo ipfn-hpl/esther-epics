6df18d8436b8c3d20f9327085834598b9bcdd65e
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2023-02-28 13:48:40 +0000

    Updated scripts
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- launch-scripts/launch-cagateway.sh
+++ launch-scripts/launch-cagateway.sh
@@ -2,8 +2,9 @@
 # https://epics.anl.gov/EpicsDocumentation/ExtensionsManuals/Gateway/Gateway.html#Starting
 EPICS_ROOT=/opt/epics
 EPICS_BASE=${EPICS_ROOT}/epics-base
-EPICS_HOST_ARCH=`${EPICS_BASE}/startup/EpicsHostArch`
+#EPICS_HOST_ARCH=`${EPICS_BASE}/startup/EpicsHostArch`
 CA_GATE_BIN=${EPICS_ROOT}/extensions/ca-gateway/bin/linux-x86_64
 MY_IP=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1)
 echo $MY_IP
-${CA_GATE_BIN}/gateway -log /tmp/gateway.log -cip 192.168.0.255 -sip ${MY_IP} -server  -archive -no_cache
+#${CA_GATE_BIN}/gateway -log /tmp/gateway.log -cip 192.168.0.255 -sip ${MY_IP} -server  -archive -no_cache
+${CA_GATE_BIN}/gateway -log /tmp/gateway.log -cip 192.168.0.255 -sip ${MY_IP} -sport 5064 -server
--- launch-scripts/launch-cs-studio.sh
+++ launch-scripts/launch-cs-studio.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 # Launch CS-Studio GUI
-cd /opt/epics/cs-studio
+#
+CSS_HOME=/opt/epics/cs-studio
+#cd /opt/epics/cs-studio
 #cd Projects/CSS/cs-studio
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/jre
-./cs-studio&
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export PATH=$JAVA_HOME/bin:$PATH
+
+$CSS_HOME/cs-studio&
 
