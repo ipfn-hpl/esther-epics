44afa658be6d0d167b919412644273c6eed85367
Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>
2025-10-23 10:14:49 +0100

    Last One pump
    Signed-off-by: Bernardo Carvalho <bernardo.carvalho@tecnico.ulisboa.pt>

--- iocs/estherVacuumRpi4/iocBoot/iocvac/st.cmd
+++ iocs/estherVacuumRpi4/iocBoot/iocvac/st.cmd
@@ -153,15 +153,15 @@ dbLoadRecords("db/nWRG.db", "P=Esther:,R=Vacuum:,PORT=LAN31,A=0")
 
 # sudo dmesg| grep FTDI
 # drvAsynSerialPortConfigure("RS232","/dev/ttyUSB1")
-drvAsynSerialPortConfigure("RS232","/dev/tty_thyracont")
-asynSetOption("RS232", 0, "baud", "9600")
-asynSetOption("RS232", 0, "bits", "8")
-asynSetOption("RS232", 0, "parity", "none")
-asynSetOption("RS232", 0, "stop", "1")
+#drvAsynSerialPortConfigure("RS232","/dev/tty_thyracont")
+#asynSetOption("RS232", 0, "baud", "9600")
+#asynSetOption("RS232", 0, "bits", "8")
+#asynSetOption("RS232", 0, "parity", "none")
+#asynSetOption("RS232", 0, "stop", "1")
 #asynSetOption("RS232", 0, "clocal", "Y")
 #asynSetOption("RS232", 0, "crtscts", "N")
 ## Load record instances
-dbLoadRecords("db/thyracontV2.db", "P=Esther:,R=Vacuum:,PORT=RS232")
+#dbLoadRecords("db/thyracontV2.db", "P=Esther:,R=Vacuum:,PORT=RS232")
 
 #- Set this to see messages from mySub
 #var mySubDebug 1
